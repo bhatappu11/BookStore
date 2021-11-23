be1c17e867bcae47c8c089f9883ab1cc9f0e5471
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-23 12:33:37 +0530

    [Arpitha]MODIFIED: added form validations to all login input fields

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import IconButton from '@mui/material/IconButton';
-// import Input from '@mui/material/Input';
-// import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-// import FormHelperText from '@mui/material/FormHelperText';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import { useForm, Controller } from "react-hook-form";
 
 const Root = styled('div')(({ theme }) => ({
     marginLeft: 80,
@@ -26,55 +22,67 @@ const Root = styled('div')(({ theme }) => ({
   
 
 function Login() {
-    const [values, setValues] = React.useState({
-        amount: '',
-        password: '',
-        weight: '',
-        weightRange: '',
-        showPassword: false,
-      });
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-      };
-    
-      const handleClickShowPassword = () => {
-        setValues({
-          ...values,
-          showPassword: !values.showPassword,
-        });
-      };
-    
-      const handleMouseDownPassword = (event) => {
-        event.preventDefault();
+      const { handleSubmit,control } = useForm();
+  
+      const onSubmit = data => {
+            console.log(data);
+      
+        };
+
+      const [passwordShown, setPasswordShown] = useState(false);
+      const togglePassword = () => {
+        setPasswordShown(!passwordShown);
       };
     return (
         <div>
             <Box sx={{margin: '25px'}}>
-                <TextField size="small" fullWidth id="outlined-basic" label="Email Id" variant="outlined" />
+              <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                    name="email"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField size="small" fullWidth id="outlined-basic" label="Email Id" variant="outlined" 
+                value={value}
+                      onChange={onChange} 
+                error={!!error}
+            helperText={error ? error.message : null}
+            type="email"/>
+            )}
+                  rules={{ required: 'Enter valid email' }}
+            />
+            <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
                 <FormControl size="small" fullWidth variant="outlined" sx={{marginTop: 2}}>
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
+                    <TextField
                         fullWidth
-                        id="outlined-adornment-password"
-                        type={values.showPassword ? 'text' : 'password'}
-                        value={values.password}
-                        onChange={handleChange('password')}
-                        endAdornment={
+                        label="Password"
+                        type={passwordShown ? "text" : "password"}
+                        value={value}
+                        onChange={onChange}
+                        error={!!error}
+                        helperText={error ? error.message : null}         
+                        InputProps={{
+                        endAdornment: (
                         <InputAdornment position="end">
                             <IconButton
                             aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
+                            onClick={togglePassword}
                             edge="end"
                             >
-                            {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                            {passwordShown ? <VisibilityOff /> : <Visibility />}
                             </IconButton>
                         </InputAdornment>
-                        }
-                        label="Password"
+                        ),}}
                     />
                 </FormControl>
-                <Button fullWidth variant="contained" style={{textTransform: 'none',background: "#A03037 0% 0% no-repeat padding-box",marginTop: 20}}>Login</Button>
+                 )}
+                 rules={{ required: 'Password required' }}
+               />
+                <Button fullWidth type="submit" variant="contained" style={{textTransform: 'none',background: "#A03037 0% 0% no-repeat padding-box",marginTop: 20}}>Login</Button>
                 <Root sx={{marginTop: 3}}> 
                     <Divider style={{fontSize: '18px',fontWeight: 'bolder'}}>OR</Divider>
                 </Root>
@@ -82,7 +90,9 @@ function Login() {
                 <Button fullWidth variant="contained" style={{textTransform: 'none',background: "#4266B2 0% 0% no-repeat padding-box"}}>Facebook</Button>
                 <Button fullWidth variant="contained" style={{textTransform: 'none',color: '#0A0102',background: "#F5F5F5 0% 0% no-repeat padding-box"}}>Google</Button>
                 </Box>
+                </form>
             </Box>
+            
         </div>
     )
 }
