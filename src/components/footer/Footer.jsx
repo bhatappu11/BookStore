66f3021d84472343b77d142373627546ce4757ef
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-24 18:53:44 +0530

    [Arpitha]ADDED: footer component for displaying footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+    return (
+        <div>
+            <div className="footer">
+                <p className="footer-content">Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
