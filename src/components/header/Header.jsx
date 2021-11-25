24a7200e860e72e12e9e52abd91fe298bb42bf36
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-25 23:30:07 +0530

    [Arpitha]ADDED: badge to cart icons to display number of items in cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,9 @@ import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import logoimage from '../../assets/education.svg';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-
+import Badge from '@mui/material/Badge';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { useSelector} from 'react-redux'
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -19,7 +21,7 @@ const Search = styled('div')(({ theme }) => ({
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
+      marginLeft: theme.spacing(8),
       width: 'auto',
     },
   }));
@@ -43,18 +45,19 @@ const Search = styled('div')(({ theme }) => ({
       transition: theme.transitions.create('width'),
       width: '100%',
       [theme.breakpoints.up('md')]: {
-        width: '20ch',
+        width: '550px',
       },
     },
   }));
 
 function Header() {
+  const items = useSelector(state=>state);
     return (
         <div>
             <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" sx={{backgroundColor: '#A03037'}}>
                 <Toolbar>
-                    <Box sx={{marginLeft: '8%',display: 'flex'}}>
+                    <Box sx={{marginLeft: '13%',display: 'flex'}}>
                         <img src={logoimage}/>
                         <Typography
                             variant="h6"
@@ -73,11 +76,17 @@ function Header() {
                     inputProps={{ 'aria-label': 'search' }}
                     />
                 </Search>
-                <Box sx={{ flexGrow: 1 }} />
-                <Box  sx={{marginRight: '10%',display: 'flex'}}>
-                    <Typography sx={{marginRight: '10%'}}>Cart</Typography>
-                    <ShoppingCartOutlinedIcon className="cart-icon" sx={{color: '#f1f1f1',cursor: 'pointer'}}/>
-                </Box>
+                <Box sx={{ flexGrow: 1}} />
+                    <Box sx={{marginRight: '1%'}}>
+                        <PersonOutlineOutlinedIcon sx={{color: '#f1f1f1',cursor: 'pointer'}}/>
+                        <Typography sx={{fontSize: '14px'}}>Account</Typography>
+                    </Box>
+                    <Box sx={{marginRight: '15%'}}>
+                      <Badge badgeContent={items.items.items.length}>
+                        <ShoppingCartOutlinedIcon sx={{color: '#f1f1f1',cursor: 'pointer'}}/>
+                      </Badge>
+                        <Typography sx={{fontSize: '14px'}} >Cart</Typography>
+                    </Box>
                 </Toolbar>
             </AppBar>
             </Box>
