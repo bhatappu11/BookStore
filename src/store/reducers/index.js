fe729d24bf103d400f5a99d6820a3565885094e1
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-26 09:42:11 +0530

    [Arpitha]ADDED: combined two reducers and exported it

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import addCartReducer from './addCartReducer'
+import wishlistReducer from './wishlistReducer'
 
 export default combineReducers({
-  items: addCartReducer
+  items: addCartReducer,
+  wishlist: wishlistReducer,
 })
