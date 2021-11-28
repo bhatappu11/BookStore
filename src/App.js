27eb10ecc3e9f9851a1ca7e2678e023dd967f23a
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-28 19:16:24 +0530

    [Arpitha]ADDED: wrapped protected route around dashboard and cart route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Account from './pages/account/Account';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Cart from './pages/cart/Cart';
+import { ProtectedRoute } from './ProtectedRoute';
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Account />}></Route>
-        <Route exact path="/dashboard" element={<Dashboard />}></Route>
-        <Route exact path="/cart" element={<Cart />}></Route>
+        <Route exact path='/' element={<ProtectedRoute/>}>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/cart" element={<Cart />}></Route>
+        </Route>
       </Routes>
     </div>
     </Router>
