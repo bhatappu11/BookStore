ea8aa3f95a8b5ba04ff0312d72eeb2f77c4f29eb
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-30 15:50:53 +0530

    [Arpitha]ADDED: wishlist page for handling wishlist items

---
+++ src/pages/wishlist/Wishlist.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import MyWishlist from '../../components/mywishlist/MyWishlist'
+
+function Wishlist() {
+    return (
+        <div>
+            <Header />
+            <MyWishlist />
+            <Footer />
+        </div>
+    )
+}
+
+export default Wishlist
