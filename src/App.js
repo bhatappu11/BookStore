423c7e6ad31eb8f777c6da59c76d77bd5d4df775
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-30 15:43:48 +0530

    [Arpitha]ADDED: route path for my wishlist page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Account from './pages/account/Account';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Cart from './pages/cart/Cart';
+import Wishlist from './pages/wishlist/Wishlist';
 import { ProtectedRoute } from './ProtectedRoute';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route exact path='/' element={<ProtectedRoute/>}>
           <Route exact path="/dashboard" element={<Dashboard />}></Route>
           <Route exact path="/cart" element={<Cart />}></Route>
+          <Route exact path="/wishlist" element={<Wishlist />}></Route>
         </Route>
       </Routes>
     </div>
